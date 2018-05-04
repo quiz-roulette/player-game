06e83f6ba76a3b3d21dba30175b3c7d6a5ec53c3
Smit <smitshah95@hotmail.com>
2018-05-05 00:24:12 +0800

    Added tata logo

--- src/Components/Home/HomeComponent.js
+++ src/Components/Home/HomeComponent.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { Jumbotron, Button } from 'react-bootstrap';
 import { withRouter } from "react-router-dom";
 import './HomeComponent.css'
+import Background from './logo.png'
 
 class HomeComponent extends Component {
     constructor(props) {
@@ -12,17 +13,15 @@ class HomeComponent extends Component {
 
     handleLearnMore(event){
         event.preventDefault();
-        this.props.history.push("/calendarview");
+        this.props.history.push("/login");
     }
 
     render() {
         return (<Jumbotron className="jumbotron">
+            <img  className="tatalogo" src={Background}/>
             <h1>Digital Tracker</h1>
             <p>
-                Designed by Digital Garage
-            </p>
-            <p>
-                <Button bsStyle="primary" onClick={this.handleLearnMore} >Calendar View</Button>
+                <Button bsStyle="primary" onClick={this.handleLearnMore} >Log In</Button>
             </p>
         </Jumbotron>);
     }
