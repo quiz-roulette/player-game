8990098c6741328493427ac6aa0261fec107bfc7
Smit <smitshah95@hotmail.com>
2018-05-18 22:16:36 +0800

    added socket

--- src/Components/Quiz/QuizListComponent.js
+++ src/Components/Quiz/QuizListComponent.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import socketIOClient from 'socket.io-client'
 import Server from '../API/server'
 import './QuizListComponent.css'
 
-class QuizListComponent extends Component{
+class QuizListComponent extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            QuizList: []
+            QuizList: [],
+            endpoint: "https://axperience.herokuapp.com/:8881"
         }
 
         Server.getQuizList().then((res) => {
@@ -18,16 +20,37 @@ class QuizListComponent extends Component{
         })
     }
 
-    render(){
+    // method for emitting a socket.io event
+    send = () => {
+        const socket = socketIOClient(this.state.endpoint)
+
+        // this emits an event to the socket (your server) with an argument of 'red'
+        // you can make the argument any color you would like, or any kind of data you want to send.
+        console.log(socket);
+        socket.emit('buzzer pressed', 'red')
+        // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
+    }
+
+    render() {
         return this.renderQuizList();
     }
 
-    renderQuizList(){
+    renderQuizList() {
         const quizList = [];
-        for(var i=0; i<this.state.QuizList.length; i++){
-            quizList.push(<div className="quizlistitem"><Link to={'/quiz/'+this.state.QuizList[i].QuizId+"/"+this.state.QuizList[i].CategoryName} >{this.state.QuizList[i].QuizId}</Link></div>)
+        const socket = socketIOClient(this.state.endpoint)
+    
+        // socket.on is another method that checks for incoming events from the server
+        // This method is looking for the event 'change color'
+        // socket.on takes a callback function for the first argument
+        socket.on('buzzer pressed', (color) => {
+          // setting the color of our button
+          console.log(color);
+          document.body.style.backgroundColor = color
+        })
+        for (var i = 0; i < this.state.QuizList.length; i++) {
+            quizList.push(<div className="quizlistitem"><Link to={'/quiz/' + this.state.QuizList[i].QuizId + "/" + this.state.QuizList[i].CategoryName} >{this.state.QuizList[i].QuizId}</Link></div>)
         }
-        return(<div className="quizlist">{quizList}</div>)
+        return (<div className="quizlist">{quizList}<br/> <button onClick={() => this.send()}>Change Color</button></div>)
     }
 }
 
