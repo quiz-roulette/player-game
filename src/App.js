1e0e276fb31382f08ef877da821174999cb9cde6
Smit <smitshah95@hotmail.com>
2018-11-21 21:26:34 +0800

    Added module to redirect for https

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect, withRouter } from 'react-router-dom';
-import { AccountComponent,AnalyticsComponent, QuizComponent, AvatarSelectionComponent, QuizListComponent, NotFoundComponent, SignUpComponent, CalendarComponent, ForgetPasswordComponent, ResetPasswordComponent, HomeComponent, DashboardComponent } from './Components'
+import { AccountComponent, AnalyticsComponent, QuizComponent, AvatarSelectionComponent, QuizListComponent, NotFoundComponent, SignUpComponent, CalendarComponent, ForgetPasswordComponent, ResetPasswordComponent, HomeComponent, DashboardComponent } from './Components'
 import Alert from 'react-s-alert';
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+import HttpsRedirect from 'react-https-redirect';
+
 
 class App extends Component {
 
@@ -16,31 +18,34 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <Router>
-                    <div className="appbody">
-                        <Switch onChange="">
-                            <Route path="/" exact component={AccountComponent} />
-                            <Route path="/home" exact component={HomeComponent} />
-                            <Route path="/calendarview" exact component={CalendarComponent} />
-                            <Route path="/quiz/:id/:categoryname" exact component={QuizComponent} />
-                            {/* <Route path="/login" exact component={SignInComponent} />
+            <HttpsRedirect>
+
+                <div className="App">
+                    <Router>
+                        <div className="appbody">
+                            <Switch onChange="">
+                                <Route path="/" exact component={AccountComponent} />
+                                <Route path="/home" exact component={HomeComponent} />
+                                <Route path="/calendarview" exact component={CalendarComponent} />
+                                <Route path="/quiz/:id/:categoryname" exact component={QuizComponent} />
+                                {/* <Route path="/login" exact component={SignInComponent} />
                             <Route path="/signup" exact component={SignUpComponent} /> */}
-                            <Route path="/account" exact component={AccountComponent}/>
-                            <Redirect from="/old-match" to="/will-match" />
-                            <Route path='/forgetPassword' component={ForgetPasswordComponent} />
-                            <Route path="/resetPassword/:uniquecode" component={ResetPasswordComponent} />
-                            <Route path='/404' component={NotFoundComponent} />
-                            <Route path='/avatar' component={AvatarSelectionComponent} />
-                            {/* <Route path="/quizlist" component={QuizListComponent} /> */}
-                            <Route path="/dashboard/:quizid" component={DashboardComponent} />
-                            <Route path="/analytics" component={AnalyticsComponent}/>
-                            <Redirect from='*' to='/404' />
-                        </Switch>
-                    </div>
-                </Router>
-                <Alert stack={{limit: 3}} />
-            </div>
+                                <Route path="/account" exact component={AccountComponent} />
+                                <Redirect from="/old-match" to="/will-match" />
+                                <Route path='/forgetPassword' component={ForgetPasswordComponent} />
+                                <Route path="/resetPassword/:uniquecode" component={ResetPasswordComponent} />
+                                <Route path='/404' component={NotFoundComponent} />
+                                <Route path='/avatar' component={AvatarSelectionComponent} />
+                                {/* <Route path="/quizlist" component={QuizListComponent} /> */}
+                                <Route path="/dashboard/:quizid" component={DashboardComponent} />
+                                <Route path="/analytics" component={AnalyticsComponent} />
+                                <Redirect from='*' to='/404' />
+                            </Switch>
+                        </div>
+                    </Router>
+                    <Alert stack={{ limit: 3 }} />
+                </div>
+            </HttpsRedirect>
         );
     }
 }
